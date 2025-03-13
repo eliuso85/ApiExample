517102f0846b2968ec2d58fd88faf32e1474bd2a
eliu cortes <eliu.cortes@intercase.com.mx>
2025-03-12 22:20:48 -0600

    update Dto USER

--- Bussines/DTOs/UserDTO.cs
+++ Bussines/DTOs/UserDTO.cs
@@ -7,8 +7,6 @@ public class UserDTO
     public int Id { get; set; }
     public required string Name { get; set; }
     public required string Email { get; set; }
-
-    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy HH:mm}")]
     public required DateTime Creation_date { get; set; }
     public required bool Active { get; set; }
 }
